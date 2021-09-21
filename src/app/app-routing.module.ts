5ed80fe10ead6a5b5efe8704f7694fd0b9f31d14
Piotr Drabik <piotr.drabik@blue-veery.com>
2021-09-21 13:55:08 +0200

    Movies Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MoviesComponent} from "./pages/movies/movies.component";
+import {PeopleComponent} from "./pages/people/people.component";
+import {SeriesComponent} from "./pages/series/series.component";
+import {MembersComponent} from "./members/members.component";
+import {AuthGuardService} from "./services/auth-guard.service";
+import {HomeComponent} from "./home/home.component";
+import {ErrorPageComponent} from "./pages/error-page/error-page.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'movies', component: MoviesComponent },
+    { path: 'people', component: PeopleComponent },
+    { path: 'series', component: SeriesComponent },
+    { path: 'members', component: MembersComponent, canActivate: [AuthGuardService] },
+    { path: '404', component: ErrorPageComponent, data: {message: 'Page not found!'} },
+    { path: '**', redirectTo: '/404' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppNavComponent } from './appnav/appnav.component';
+import { MaterialModule } from './material-module';
+import { MoviesComponent } from './pages/movies/movies.component';
+import { SeriesComponent } from './pages/series/series.component';
+import { PeopleComponent } from './pages/people/people.component';
+import { MembersComponent } from './members/members.component';
+import { HomeComponent } from './home/home.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { AlertHelperComponent } from './alert-helper/alert-helper.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        AppNavComponent,
+        MoviesComponent,
+        SeriesComponent,
+        PeopleComponent,
+        MembersComponent,
+        HomeComponent,
+        ErrorPageComponent,
+        AlertHelperComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MaterialModule,
+    ],
+    providers: [
+        AlertHelperComponent
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
