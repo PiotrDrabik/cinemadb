fe6a83f0af990cb9d943edaa10251743f1d8a759
Piotr Drabik <piotr.drabik@blue-veery.com>
2021-09-22 18:37:24 +0200

    add ngx-translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -32,6 +34,14 @@ import { AlertHelperComponent } from './resources/alert-helper/alert-helper.comp
         AppRoutingModule,
         BrowserAnimationsModule,
         MaterialModule,
+        HttpClientModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        })
     ],
     providers: [
         AlertHelperComponent
@@ -39,3 +49,8 @@ import { AlertHelperComponent } from './resources/alert-helper/alert-helper.comp
     bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+    return new TranslateHttpLoader(http);
+}
