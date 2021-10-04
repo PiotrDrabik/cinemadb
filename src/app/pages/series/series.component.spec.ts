c06963b089a17b69d3d4dd5c9818e9b374b66bc0
Piotr Drabik <piotr.drabik@blue-veery.com>
2021-10-04 08:47:09 +0200

    series component and unit tests

--- src/app/pages/series/series.component.spec.ts
+++ src/app/pages/series/series.component.spec.ts
@@ -1,6 +1,12 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
 import { SeriesComponent } from './series.component';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { RouterTestingModule } from "@angular/router/testing";
+import { TranslateModule } from "@ngx-translate/core";
+import { HttpService } from "../../services/http.service";
+import { AppStateService } from "../../services/app-state.service";
+import { AppConfigService } from "../../services/app-config.service";
+import { AlertHelperComponent } from "../../resources/alert-helper/alert-helper.component";
 
 describe('SeriesComponent', () => {
     let component: SeriesComponent;
@@ -8,9 +14,11 @@ describe('SeriesComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [ SeriesComponent ]
+            declarations: [ SeriesComponent ],
+            imports: [ HttpClientTestingModule, RouterTestingModule, TranslateModule.forRoot() ],
+            providers: [ HttpService, AppStateService, AppConfigService, AlertHelperComponent ]
         })
-            .compileComponents();
+        .compileComponents();
     });
 
     beforeEach(() => {
@@ -22,4 +30,32 @@ describe('SeriesComponent', () => {
     it('should create', () => {
         expect(component).toBeTruthy();
     });
+
+    it('test fetchFromObservable() queryParams', fakeAsync(() => {
+        // @ts-ignore
+        component.getData = (queryCheck) => {
+
+            expect(queryCheck).not.toEqual(jasmine.objectContaining({
+                test: 'dummy'
+            }));
+            expect(queryCheck).toEqual(jasmine.objectContaining({
+                baseAction: 'trending'
+            }));
+            expect(queryCheck).toEqual(jasmine.objectContaining({
+                mediaType: 'tv/day'
+            }));
+            expect(queryCheck).toEqual(jasmine.objectContaining({
+                language: 'en-EN'
+            }));
+            expect(queryCheck).toEqual(jasmine.objectContaining({
+                query: undefined
+            }));
+            expect(queryCheck).toEqual(jasmine.objectContaining({
+                page: undefined
+            }));
+
+            return { subscribe: () => {} }
+        };
+        component.fetchFromObservable();
+    }));
 });
--- src/app/pages/series/series.component.ts
+++ src/app/pages/series/series.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { QueryUrlModel } from "../../models/query-url-model";
+import { BaseRes } from "../../models/base-response";
+import { BuilderDataComponent } from "../../base-class/builder-data/builder-data.component";
+import { HttpService } from "../../services/http.service";
+import { AppConfigService } from "../../services/app-config.service";
+import { AppStateService } from "../../services/app-state.service";
+import { Router } from "@angular/router";
+import { PersonResponseModel } from "../../models/person-response-model";
+import { MediaResponseModel } from "../../models/media-response-model";
 
 @Component({
     selector: 'app-series',
     templateUrl: './series.component.html',
     styles: []
 })
-export class SeriesComponent implements OnInit {
+export class SeriesComponent extends BuilderDataComponent {
 
-    constructor() { }
-
-    ngOnInit(): void {
+    constructor(public http: HttpService,
+                public app: AppConfigService,
+                public state: AppStateService,
+                public router: Router) {
+        super(http, app, state, router)
     }
 
+    fetchFromObservable() {
+        let queryUrl: QueryUrlModel = {
+            baseAction: 'trending',
+            mediaType: 'tv/day',
+            language: this.app.contentLanguage,
+            query: undefined,
+            page: undefined
+        }
+
+        this.getData(queryUrl).subscribe( (observer: BaseRes[] | PersonResponseModel | MediaResponseModel) => {
+            if (Array.isArray(observer)) {
+                this.state.series = observer;
+            }
+        })
+    }
 }
