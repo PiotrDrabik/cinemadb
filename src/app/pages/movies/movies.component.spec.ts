18ae1b85e8f46e7a6f9288dfa9e9c6af4ee83de7
Piotr Drabik <piotr.drabik@blue-veery.com>
2021-10-04 08:44:31 +0200

    movies component and unit tests

--- src/app/pages/movies/movies.component.spec.ts
+++ src/app/pages/movies/movies.component.spec.ts
@@ -1,25 +1,61 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
 import { MoviesComponent } from './movies.component';
+import { AppStateService } from "../../services/app-state.service";
+import { AppConfigService } from "../../services/app-config.service";
+import { HttpService } from "../../services/http.service";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { AlertHelperComponent } from "../../resources/alert-helper/alert-helper.component";
+import { RouterTestingModule } from "@angular/router/testing";
+import { TranslateModule } from "@ngx-translate/core";
 
 describe('MoviesComponent', () => {
-  let component: MoviesComponent;
-  let fixture: ComponentFixture<MoviesComponent>;
+    let component: MoviesComponent;
+    let fixture: ComponentFixture<MoviesComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ MoviesComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ MoviesComponent ],
+            imports: [ HttpClientTestingModule, RouterTestingModule, TranslateModule.forRoot() ],
+            providers: [ HttpService, AppStateService, AppConfigService, AlertHelperComponent ]
+        })
+        .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MoviesComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(MoviesComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('test fetchFromObservable() queryParams', fakeAsync(() => {
+        // @ts-ignore
+        component.getData = (queryCheck) => {
+
+            expect(queryCheck).not.toEqual(jasmine.objectContaining({
+                test: 'dummy'
+            }));
+            expect(queryCheck).toEqual(jasmine.objectContaining({
+                baseAction: 'trending'
+            }));
+            expect(queryCheck).toEqual(jasmine.objectContaining({
+                mediaType: 'movie/day'
+            }));
+            expect(queryCheck).toEqual(jasmine.objectContaining({
+                language: 'en-EN'
+            }));
+            expect(queryCheck).toEqual(jasmine.objectContaining({
+                query: undefined
+            }));
+            expect(queryCheck).toEqual(jasmine.objectContaining({
+                page: undefined
+            }));
+
+            return { subscribe: () => {} }
+        };
+        component.fetchFromObservable();
+    }));
 });
--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -1,16 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { QueryUrlModel } from "../../models/query-url-model";
+import { BaseRes } from "../../models/base-response";
+import { BuilderDataComponent } from "../../base-class/builder-data/builder-data.component";
+import { HttpService } from "../../services/http.service";
+import { AppConfigService } from "../../services/app-config.service";
+import { AppStateService } from "../../services/app-state.service";
+import { Router } from "@angular/router";
+import { PersonResponseModel } from "../../models/person-response-model";
+import { MediaResponseModel } from "../../models/media-response-model";
 
 @Component({
     selector: 'app-movies',
     templateUrl: './movies.component.html',
     styles: []
 })
-export class MoviesComponent implements OnInit {
+export class MoviesComponent extends BuilderDataComponent {
 
-    constructor() { }
-
-    ngOnInit(): void {
-
+    constructor(public http: HttpService,
+                public app: AppConfigService,
+                public state: AppStateService,
+                public router: Router) {
+        super(http, app, state, router)
     }
 
+    fetchFromObservable() {
+        let queryUrl: QueryUrlModel = {
+            baseAction: 'trending',
+            mediaType: 'movie/day',
+            language: this.app.contentLanguage,
+            query: undefined,
+            page: undefined
+        }
+
+        this.getData(queryUrl).subscribe( (observer: BaseRes[] | PersonResponseModel | MediaResponseModel) => {
+            if (Array.isArray(observer)) {
+                this.state.movies = observer;
+            }
+        })
+    }
 }
