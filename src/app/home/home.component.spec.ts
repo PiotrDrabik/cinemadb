443900e6770a85adb8813e7d7cf0b15433927698
Piotr Drabik <piotr.drabik@blue-veery.com>
2021-10-04 11:20:58 +0200

    home component and unit tests

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,25 +1,91 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { RouterTestingModule } from "@angular/router/testing";
+import { TranslateModule } from "@ngx-translate/core";
+import { HttpService } from "../services/http.service";
+import { AppStateService } from "../services/app-state.service";
+import { AppConfigService } from "../services/app-config.service";
+import { AlertHelperComponent } from "../resources/alert-helper/alert-helper.component";
 
 describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+    let component: HomeComponent;
+    let fixture: ComponentFixture<HomeComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ HomeComponent ],
+            imports: [ HttpClientTestingModule, RouterTestingModule, TranslateModule.forRoot() ],
+            providers: [ HttpService, AppStateService, AppConfigService, AlertHelperComponent ]
+        })
+        .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(HomeComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('test fetchFromObservable() queryParams', fakeAsync(() => {
+        // overwrite getAdditionalData it will be tested in another scenario
+        component.getAdditionalData = () => {};
+        // @ts-ignore
+        component.getData = (queryCheck) => {
+
+            expect(queryCheck).not.toEqual(jasmine.objectContaining({
+                test: 'dummy'
+            }));
+            expect(queryCheck).toEqual(jasmine.objectContaining({
+                baseAction: 'trending'
+            }));
+            expect(queryCheck).toEqual(jasmine.objectContaining({
+                mediaType: 'all/day'
+            }));
+            expect(queryCheck).toEqual(jasmine.objectContaining({
+                language: 'en-EN'
+            }));
+            expect(queryCheck).toEqual(jasmine.objectContaining({
+                query: undefined
+            }));
+            expect(queryCheck).toEqual(jasmine.objectContaining({
+                page: undefined
+            }));
+
+            return { subscribe: () => {} }
+        };
+        component.fetchFromObservable();
+    }));
+
+    it('test getAdditionalData() queryParams', fakeAsync(() => {
+        // @ts-ignore
+        component.getData = (queryCheck) => {
+
+            expect(queryCheck).not.toEqual(jasmine.objectContaining({
+                test: 'dummy'
+            }));
+            expect(queryCheck).toEqual(jasmine.objectContaining({
+                baseAction: 'discover'
+            }));
+            expect(queryCheck).toEqual(jasmine.objectContaining({
+                mediaType: 'movie'
+            }));
+            expect(queryCheck).toEqual(jasmine.objectContaining({
+                language: 'en-EN'
+            }));
+            expect(queryCheck).toEqual(jasmine.objectContaining({
+                query: '&region=PL&sort_by=release_date.asc'
+            }));
+            expect(queryCheck).toEqual(jasmine.objectContaining({
+                page: undefined
+            }));
+
+            return { subscribe: () => {} }
+        };
+        component.getAdditionalData();
+    }));
 });
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,35 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { HttpService } from "../services/http.service";
 import { AppConfigService } from "../services/app-config.service";
 import { AppStateService } from "../services/app-state.service";
-import {map} from "rxjs/operators";
-import {Home} from "../models/home-response";
-
-interface Response {
-    results: []
-}
+import { BaseRes } from "../models/base-response";
+import { Router } from "@angular/router";
+import { Subscription } from "rxjs";
+import { QueryUrlModel } from "../models/query-url-model";
+import { BuilderDataComponent } from "../base-class/builder-data/builder-data.component";
+import { PersonResponseModel } from "../models/person-response-model";
+import { MediaResponseModel } from "../models/media-response-model";
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
-    styles: []
+    styleUrls: ['home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent extends BuilderDataComponent {
+    public refreshSub: Subscription;
 
-    constructor(private http: HttpService, private app: AppConfigService, public state: AppStateService) { }
-
-    ngOnInit(): void {
-        this.http.getData(`https://api.themoviedb.org/3/trending/all/day?api_key=${this.app.apiKey}&language=${this.app.contentLanguage}`)
-            .pipe(
-                map( (res: any): Home[] => {
-                    console.log('res', res);
-                    return res.results;
-                })
-            )
-            .subscribe( (observer: Home[]) => {
-                this.state.home = observer;
-                console.log('--- observer', observer);
-        })
+    constructor(public http: HttpService,
+                public app: AppConfigService,
+                public state: AppStateService,
+                public router: Router) {
+        super(http, app, state, router)
     }
 
+    fetchFromObservable() {
+        let queryTrending: QueryUrlModel = {
+            baseAction: 'trending',
+            mediaType: 'all/day',
+            language: this.app.contentLanguage,
+            query: undefined,
+            page: undefined
+        };
+
+        this.getData(queryTrending).subscribe( (observer: BaseRes[] | PersonResponseModel | MediaResponseModel) => {
+            if (Array.isArray(observer)) {
+                this.state.home = observer;
+            }
+        })
+        this.getAdditionalData();
+    }
+
+    getAdditionalData() {
+        let today = new Date();
+        let todayFormatted = `${today.getFullYear()}-`+
+                            `${String(today.getMonth() + 1).padStart(2, '0')}-`+
+                            `${String(today.getDate()).padStart(2, '0')}`
+        let queryDiscover: QueryUrlModel = {
+            baseAction: 'discover',
+            mediaType: 'movie',
+            language: this.app.contentLanguage,
+            query: '&region=PL&sort_by=release_date.asc',
+            append: `&release_date.gte=${todayFormatted}&with_watch_monetization_types=flatrate`,
+            page: undefined
+        };
+        this.getData(queryDiscover).subscribe( (observer: BaseRes[] | PersonResponseModel | MediaResponseModel) => {
+            if (Array.isArray(observer)) {
+                this.state.discover = observer;
+            }
+        })
+    }
 }
